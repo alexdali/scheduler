431d49091c4973934a42c2bc791704aed21a05d7
alexeytas <alexdali@gmail.com>
2018-12-12 11:31:08 +0600

    added HTML markup

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -9,10 +9,58 @@ import Spinner from 'components/Spinner';
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
+const Tasks = [
+    {
+        id: 1,
+        completed: false,
+        favorite: false,
+        message: 'first',
+    }, 
+    {
+        id: 2,
+        completed: false,
+        favorite: false,
+        message: 'Second',
+    }, 
+    {
+        id: 3,
+        completed: false,
+        favorite: false,
+        message: 'Thirty',
+    }, 
+    {
+        id: 4,
+        completed: false,
+        favorite: false,
+        message: 'Fourty',
+    }, 
+];
+
 export default class Scheduler extends Component {
+    //const searchTask;
+
     render () {
         return (
             <section className = { Styles.scheduler }>
+                <main>
+                    <header>
+                        <h1>
+                            Планировщик задач
+                        </h1>
+                        <form onSubmit = { this._handleFormSubmit }>
+                            <textarea
+                                placeholder = { `What's on your mind` }
+                                value = ''//{ searchTask }
+                                // onChange = { this._updateComment }
+                                // onKeyPress = { this._submitOnEnter }
+                            />
+                            <input
+                                type = 'submit'
+                                value = 'Search'
+                            />
+                        </form>
+                    </header>
+                </main>
                 <Task />
                 <Spinner />
             </section>
